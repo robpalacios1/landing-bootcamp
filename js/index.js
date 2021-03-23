35a0fae73d3264aaf9835948676ceeb105f49b5a
Tatiana Pena <tatiana.pena@koombea.com>
2021-03-23 11:47:05 -0500

    put fixed header

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector(".js-burger-button")
 const burgerClose = document.querySelector(".js-burger-close")
 const header = document.querySelector(".js-header")
+const stickyHeader = document.querySelector(".js-sticky")
 
 //function for burger button click
 burger.addEventListener('click', () => {
@@ -22,3 +23,18 @@ function headerResize() {
 }
 
 window.addEventListener('resize',headerResize);
+
+
+// function for header sticky
+
+window.onscroll = function() {myFunction()};
+
+const sticky = stickyHeader.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    stickyHeader.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
